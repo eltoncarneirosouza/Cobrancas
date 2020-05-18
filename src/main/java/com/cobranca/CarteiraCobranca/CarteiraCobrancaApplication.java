a6bddf4e80e0cc5546b48730725108569861a875
eltoncarneirosouza <elton.phone@hotmail.com>
2020-05-18 20:22:35 -0300

    upload inicial

---
+++ src/main/java/com/cobranca/CarteiraCobranca/CarteiraCobrancaApplication.java
@@ -0,0 +1,13 @@
+package com.cobranca.CarteiraCobranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraCobrancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteiraCobrancaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cobranca/CarteiraCobranca/CarteiraCobrancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cobranca.CarteiraCobranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraCobrancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
