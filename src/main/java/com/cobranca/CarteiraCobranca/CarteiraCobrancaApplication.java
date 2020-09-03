cf52592353c7bc5a4f34e3030cb9c64218a25a01
eltoncarneirosouza <elton.phone@hotmail.com>
2020-09-02 21:31:32 -0300

    02-09-2020

--- src/main/java/com/cobranca/CarteiraCobranca/CarteiraCobrancaApplication.java
+++ src/main/java/com/cobranca/CarteiraCobranca/CarteiraCobrancaApplication.java
@@ -1,15 +1,17 @@
 package com.cobranca.CarteiraCobranca;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class CarteiraCobrancaApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(CarteiraCobrancaApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(CarteiraCobrancaApplication.class, args);
-		System.out.println("senha-->"+new BCryptPasswordEncoder().encode("123456"));
+		logger.info("====Inicializando o sistema====");
 	}
 
 }
